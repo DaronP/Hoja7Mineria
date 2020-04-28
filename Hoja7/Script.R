733dba8f4509f373f66f15bbe0e1b5f07478f1b0
DaronP <jandresperez1@yahoo.com>
2020-04-27 22:13:31 -0600

    Intento de SVM

--- Hoja7/Script.R
+++ Hoja7/Script.R
@@ -3,6 +3,7 @@ install.packages("RWeka")
 install.packages("neuralnet")
 install.packages("neural")
 install.packages("car")
+install.packages("e1071")
 
 library(caret)
 library(nnet)
@@ -11,36 +12,49 @@ library(neural)
 library(dummy)
 library(neuralnet)
 library(car)
+library(e1071)
 
 
 dataset<-read.csv("train.csv")
 
+set.seed(123)
+
 #---------Variable categorica-------
-dataset$SalePrice_C<-recode(dataset$SalePrice, "lo:12664=1;12665:235197=2;235198:hi=3")
+dataset$SalePrice_C<-recode(dataset$SalePrice, "lo:126520=1;126521:234963=2;234964:hi=3")
 
 corte<-sample(nrow(dataset),nrow(dataset)*0.7)
 
 train<-dataset[corte,]
 test<-dataset[-corte,]
 
-dataset.SaleCondition
 
 
 
-#---------------Con nnet-----------------------------------
-modelo.nn2 <- nnet(train$SalePrice_C~.,data = train, subset = corte, size=2, rang=0.1, decay=5e-4, maxit=200) 
-prediccion2 <- as.data.frame(predict(modelo.nn2, newdata = test[,c(44,47,82)]))
-columnaMasAlta<-apply(prediccion2, 1, function(x) colnames(prediccion2)[which.max(x)])
-test$prediccion2<-columnaMasAlta
-
-cfm<-confusionMatrix(as.factor(test$prediccion2),test$SalePrice_C)
-cfm
+#---------------Con caret-----------------------------------
+modeloCaret <- train(SalePrice_C~GrLivArea+X1stFlrSF, data = train, method="nnet", trace=F)
+test$prediccionCaret<-predict(modeloCaret, newdata = test[,c(44,47,82)])
+cfmCaret<-confusionMatrix(as.factor(test$prediccionCaret),as.factor(test$SalePrice_C))
+cfmCaret
 
 
 #------------Con NeuralNet--------------
 
-modelo.nn <- neuralnet(train$SalePrice_C~train$GrLivArea+train$X1stFlrSF, train[,c(44,47,82)], hidden = 2, rep = 3)
+modelo.nn <- neuralnet(SalePrice_C~GrLivArea+X1stFlrSF, data = train, hidden = 4, rep = 2)
 plot(modelo.nn, newdata=test) 
 test$predNeuralNet<-round(predict(modelo.nn,newdata = test[,c(44,47,82)]),0)
 cfmNeuralNet<-confusionMatrix(as.factor(test$predNeuralNet),as.factor(test$SalePrice_C))
 cfmNeuralNet
+
+#------------SVM---------------
+mdodeloSVM1 <- svm(SalePrice~GrLivArea+X1stFlrSF, data = train, kernel = "polynomial", cost = 0.1)
+mdodeloSVM1
+plot(mdodeloSVM1, train[,corte])
+predSVM1 <- predict(mdodeloSVM1, newdata = test, type = "class")
+plot(predSVM1)
+
+
+mdodeloSVM2 <- svm(SalePrice_C~GrLivArea+X1stFlrSF, data = train, kernel = "polynomial")
+predSVM2 <- predict(mdodeloSVM1, newdata = test)
+
+
+
